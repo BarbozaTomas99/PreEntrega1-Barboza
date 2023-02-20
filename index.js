eb315bc329bcedb2130a2610e6fdca07b59e9bed
Tomás Barboza <116506342+BarbozaTomas99@users.noreply.github.com>
2023-02-20 17:46:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 // CALCULADORA DOLAR TARJETA //
 
-let valorDolar;
+let cotizacionDolar = parseInt (prompt ("Ingrese el valor de la cotizacion (Pesos)"));
+
 function calcularValores(){
-    valorDolar = parseInt (prompt ("Ingrese cantidad de dólares (USD)"));
-    let valorPesos = (valorDolar * 189.75); //Valor dolar actual 20/01/2023
+    let valorDolar = parseInt (prompt ("Ingrese cantidad de dólares (USD)"));
+    let valorPesos = (valorDolar * cotizacionDolar);
     let impuestoPais = (valorPesos * 30 / 100); //El impuesto es del 30%
-    let impuestoGanacias = (valorPesos * 45 / 100); //El impuesto es del 45%
-    let total = (valorPesos + impuestoPais + impuestoGanacias);
-    alert ("Costo en Pesos: " + valorPesos + ", Impuesto país del 30%: " + impuestoPais + ", Impuesto a las ganancias del 45%: " + impuestoGanacias + ", Total: " + total);
+    let impuestoGanancias = (valorPesos * 45 / 100); //El impuesto es del 45%
+    let total = (valorPesos + impuestoPais + impuestoGanancias);
+    alert("Costo en Pesos: " + valorPesos + "\nImpuesto país del 30%: " + impuestoPais + "\nImpuesto a las ganancias del 45%: " + impuestoGanancias + "\nTotal: " + total);
 }
-function continuar(){
-    let pregunta = prompt ("¿Desea ingresar otro valor? S/N");
-    switch (pregunta){
-        case "S":
-            calcularValores();
-            continuar();
-            break;
-        case "N":
-            alert ("Gracias por usar esta calculadora");
-            break;
-        default:
-            alert ("No se reconoce el comando, por favor ingresar solo S/N");
-            continuar();
-            break
+
+// Ejecutar la función principal
+
+calcularValores ();
+
+let pregunta = "S";
+while (pregunta === "S") {
+    pregunta = prompt("Desea ingresar otro valor? S/N");
+    if (pregunta === "S") {
+        calcularValores();
     }
 }
-calcularValores ();
-continuar();
 
-
+alert("Gracias por usar esta calculadora");
